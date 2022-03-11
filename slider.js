4f31fe11487698a164479fe84c9aadb108730cc5
hasan-ullah <mdhasanullah124@gmail.com>
2022-03-11 20:35:58 +0600

    slider ready

---
+++ slider.js
@@ -0,0 +1,13 @@
+const img =[
+    'img/pic-1.jpg',
+    'img/pic-2.jpg',
+    'img/pic-3.jpg',
+    'img/pic-4.jpg',
+    'img/pic-5.jpg',
+    'img/pic-6.jpg'
+    
+]
+
+setInterval(()=>{
+
+},1000)
